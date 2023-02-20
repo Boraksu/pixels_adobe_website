84d0b6c6b5c22c6143c952771a7b330b72263728
Boraksu <113847071+Boraksu@users.noreply.github.com>
2023-02-20 08:27:41 +0000

    Fix unused imports

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,4 +1,28 @@
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/e
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { CdkTableModule } from '@angular/cdk/table';
+
+@NgModule({
+    exports: [
+        MatToolbarModule,
+        MatExpansionModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatTableModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatIconMod
