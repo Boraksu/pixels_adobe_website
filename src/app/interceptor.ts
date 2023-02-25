5336bf55016404955b50ff50e255ba449a3ae8aa
Boraksu <113847071+Boraksu@users.noreply.github.com>
2023-02-25 22:40:47 +0000

    security

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -7,4 +7,28 @@ import {
 } from '@angular/common/http';
 
 import {Observable, throwError} from 'rxjs';
-impo
+import {catchError, retry} from 'rxjs/operators';
+import {MatDialog} from '@angular/material/dialog';
+import {ErrorComponent} from './error/error.component';
+
+/**
+ * Pass untouched request through to the next request handler.
+ **/
+@Injectable()
+export class ServerInterceptor implements HttpInterceptor {
+
+    constructor(private dialog: MatDialog) {
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const auxReq = req.clone({
+            url: `http://localhost:3000${req.url}`
+        });
+        return next.handle(auxReq).pipe(
+            retry(1),
+            catchError((error: HttpErrorResponse) => {
+                let errorMessage = '';
+                if (error.error instanceof ErrorEvent) {
+                    errorMessage = `Client side error Message: ${error.error.message}`;
+                } else {
+       
