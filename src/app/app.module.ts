4cf4023492a7fe212f25b15b98c5acb56dc6dad6
Boraksu <113847071+Boraksu@users.noreply.github.com>
2023-02-25 07:06:29 +0000

    net: mvneta: Fix CPU_MAP registers initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,2 +1,33 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@a
+import {NgModule} from '@angular/core';
+
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {ServerInterceptor} from './interceptor';
+
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// angular material module
+import { AngularMaterialModule } from './angular-material.module';
+
+// Components
+import { StockModule } from './stock/stock.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ErrorComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        AngularMaterialModule,
+        StockModule
+    ],
+    providers: [{provide: HTTP_INTERCE
