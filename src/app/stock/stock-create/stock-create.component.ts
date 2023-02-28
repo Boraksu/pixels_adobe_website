66ed1b1ec9a8395b57a7f9947843282297cc8c8b
Boraksu <113847071+Boraksu@users.noreply.github.com>
2023-02-28 21:37:46 +0000

    Remove comments

--- src/app/stock/stock-create/stock-create.component.ts
+++ src/app/stock/stock-create/stock-create.component.ts
@@ -1,3 +1,24 @@
 import { Component } from '@angular/core';
 import { StocksService } from '../stocks.service';
-import { NgForm } from '
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+    selector: 'app-stock-create',
+    templateUrl: './stock-create.component.html',
+    styleUrls: ['./stock-create.component.css']
+})
+export class StockCreateComponent {
+    panelOpenState = false;
+
+    constructor(public stocksService: StocksService) {
+    }
+
+    onAddStock(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
+        this.stocksService.addStock(form.value.name, form.value.price, form.value.stock);
+        form.resetForm();
+    }
+}
---
+++ src/app/stock/stock-list/stock-list.component.spec.ts
@@ -0,0 +1,10 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StockListComponent } from './stock-list.component';
+
+describe('StockListComponent', () => {
+  let component: StockListComponent;
+  let fixture: ComponentFixture<StockListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingMod
