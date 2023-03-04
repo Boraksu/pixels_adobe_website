a42e52b36dd277d66cc5cf0666300f908573ae91
Boraksu <113847071+Boraksu@users.noreply.github.com>
2023-03-04 21:57:36 +0000

    added dist

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,4 +19,17 @@
 
  /*
  * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- 
+ * with the following flag, it will bypass `zone.js` patch for IE/Edge
+ */
+// (window as any).__Zone_enable_cross_context_check = true;
+
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
